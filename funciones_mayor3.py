8fe25fd77c7676dc6982a586401eef1517abc31b
Hector Jimenez <70037077+emitazz77@users.noreply.github.com>
2025-12-25 22:20:01 -0300

    Add files via upload

---
+++ funciones_mayor3.py
@@ -0,0 +1,16 @@
+def mayor_tres(num1,num2,num3):
+    if num1>num2 and num1>num3:
+    print(f'es mayor {num1}')
+    elif num2>num1 and num2>num3:
+    print(f'es mayor el num, {num2}')
+    else:
+    print(f"el mayor es {num3}")
+    
+mayor_tres(1,4,5)
+    
+    
+    
+    
+    
+    
+    
